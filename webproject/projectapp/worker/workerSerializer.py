976e8978bc91c07691a14070d829d39d51ddaccf
KingDzx <zachary.orr@gmail.com>
2018-04-25 20:06:24 -0400

    smeh

--- webproject/projectapp/worker/workerSerializer.py
+++ webproject/projectapp/worker/workerSerializer.py
@@ -4,8 +4,4 @@ from rest_framework import serializers
 class WorkerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Worker
-<<<<<<< HEAD
         fields = ('id','first_name','last_name','username','password')
-=======
-        fields = ('id','name','service','address','description')
->>>>>>> 98e27fde1844552500715dbe10a963ea2ef74657
